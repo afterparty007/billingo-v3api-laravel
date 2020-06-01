00e69b1b8885ee21ed6d1884e80e832c51ee2bed
Kristof <tokfozelek007@gmail.com>
2020-06-01 11:08:01 +0200

    spervice provider

---
+++ lib/ServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Swagger\Client;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+     protected $defer = false;
+
+     /**
+      * Register the service provider.
+      *
+      * @return void
+      */
+     public function register()
+     {}
+
+     /**
+      * Register the service provider.
+      *
+      * @return void
+      */
+     public function boot()
+     {}
+    
+}
